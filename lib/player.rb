ea8b83ab9c2daa1f286881ca5f69a8d27e3e2acf
ParishBen <ParishBen13@gmail.com>
2020-06-28 00:39:40 -0400

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 class Player
     attr_reader :token
   
     def initialize(token)
       @token = token
     end
-=======
-class Player
-    attr_reader :token
-  
-    def initialize(token)
-      @token = token
-    end
->>>>>>> bc70a24e23b108c0a81537d536836fbf6009de87
   end
