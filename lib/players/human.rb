f0bf95d6a25ef0e7805cf53a8f176bca3dad999a
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-28 04:31:36 +0000

    Done.

---
+++ lib/players/human.rb
@@ -0,0 +1,14 @@
+module Players
+  class Human < Player
+
+
+    def move(board)
+      input = gets.strip
+    end
+  end
+end
+
+#The human player must implement a #move method that takes in a board argument and allows a human player to enter a move via the CLI.
+#The method should return the value the user enters.
+# Even though the method accepts a board argument, it does not need to use it.
+
