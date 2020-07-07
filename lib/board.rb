fe099ee0a220fbb18a8afaf80207dac996983ef0
Jennifer Prince <jennifergraceprince@gmail.com>
2020-07-07 06:09:51 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -34,5 +34,10 @@ class Board
   end
   
   def valid_move?(input)
+    input.to_i.between?(1, 9) && !taken?(input)
+  end
+  
+  def update(input, player)
+    cells[input.to_i-1] = player.token 
   end
 end
